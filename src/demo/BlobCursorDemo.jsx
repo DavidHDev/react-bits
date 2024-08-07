ee8bb44e07a4709ed6246bf73b5237ad2884e883
David <haz.d@outlook.com>
2024-08-07 21:20:45 +0300

    add extra info for blob cursor component

--- src/demo/BlobCursorDemo.jsx
+++ src/demo/BlobCursorDemo.jsx
@@ -2,16 +2,21 @@ import { Box } from "@chakra-ui/react";
 import BlobCursor from "../content/Animations/BlobCursor/BlobCursor";
 import CodeExample from "../components/CodeExample";
 import { CODE_EXAMPLES } from "../constants/ExampleConstants";
+import { WarningIcon } from "@chakra-ui/icons";
 
 const BlobCursorDemo = () => {
   const { blobCursor } = CODE_EXAMPLES;
 
   return (
     <>
-      <h2 className="demo-title">Demo (hover)</h2>
+      <h2 className="demo-title">Demo</h2>
       <Box height={200} position="relative" className="demo-container" overflow="hidden">
         <BlobCursor />
       </Box>
+
+      <p className="demo-extra-info">
+        <WarningIcon position="relative" /> Not supported on Safari
+      </p>
 
       <p className="demo-details">
         This component uses <span>@react-spring/web</span> for the animation.
